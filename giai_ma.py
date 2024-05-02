279cfeef3b47e73c952e6360b2e54f8efa01afc6
nbathien <148745036+nbathien@users.noreply.github.com>
2024-05-02 22:37:17 +0700

    Update giai_ma.py

--- giai_ma.py
+++ giai_ma.py
@@ -1,20 +1,27 @@
 # Hàm giải mã Playfair
 def playfair_decrypt(cipher_text, key):
+    # Chuyển đổi văn bản thành chữ thường
     def toLowerCase(plain):
         return plain.lower()
 
+    # Loại bỏ khoảng trắng trong văn bản
     def removeSpaces(plain):
         return ''.join(plain.split())
 
+    # Tạo ma trận khóa từ key
     def generateKeyTable(key):
+        # Khởi tạo ma trận khóa rỗng
         keyT = [['' for i in range(5)] for j in range(5)]
+        # Khởi tạo từ điển để theo dõi việc sử dụng ký tự trong key
         dicty = {chr(i + 97): 0 for i in range(26)}
 
+        # Đánh dấu các ký tự trong key (trừ 'j')
         for i in range(len(key)):
             if key[i] != 'j':
                 dicty[key[i]] = 2
         dicty['j'] = 1
 
+        # Điền các ký tự từ key vào ma trận khóa
         i, j, k = 0, 0, 0
         while k < len(key):
             if dicty[key[k]] == 2:
@@ -26,6 +33,7 @@ def playfair_decrypt(cipher_text, key):
                     j = 0
             k += 1
 
+        # Điền các ký tự còn lại vào ma trận khóa
         for k in dicty.keys():
             if dicty[k] == 0:
                 keyT[i][j] = k
@@ -36,6 +44,7 @@ def playfair_decrypt(cipher_text, key):
 
         return keyT
 
+    # Tìm vị trí của các ký tự trong ma trận khóa
     def search(keyT, a, b):
         arr = [0, 0, 0, 0]
 
@@ -53,32 +62,39 @@ def playfair_decrypt(cipher_text, key):
 
         return arr
 
+    # Hàm lấy phần dư khi chia cho 5
     def mod5(a):
         if a < 0:
             a += 5
         return a % 5
 
+    # Hàm giải mã văn bản
     def decrypt(str, keyT):
         ps = len(str)
         i = 0
         while i < ps:
             a = search(keyT, str[i], str[i+1])
-            if a[0] == a[2]:
+            if a[0] == a[2]:  # Cùng hàng
                 str = str[:i] + keyT[a[0]][mod5(a[1]-1)] + keyT[a[0]][mod5(a[3]-1)] + str[i+2:]
-            elif a[1] == a[3]:
+            elif a[1] == a[3]:  # Cùng cột
                 str = str[:i] + keyT[mod5(a[0]-1)][a[1]] + keyT[mod5(a[2]-1)][a[1]] + str[i+2:]
-            else:
+            else:  # Khác hàng và cột
                 str = str[:i] + keyT[a[0]][a[3]] + keyT[a[2]][a[1]] + str[i+2:]
             i += 2
 
         return str
 
+    # Hàm giải mã Playfair chính
     def decryptByPlayfairCipher(str, key):
-        ks = len(key)
+        # Chuẩn hóa key và văn bản đầu vào
         key = removeSpaces(toLowerCase(key))
         str = removeSpaces(toLowerCase(str))
+        # Tạo ma trận khóa
         keyT = generateKeyTable(key)
+        # Giải mã văn bản
         return decrypt(str, keyT)
 
+    # Giải mã văn bản đã mã hóa bằng mật mã Playfair
     decrypted_text = decryptByPlayfairCipher(cipher_text, key)
     return decrypted_text
+    
